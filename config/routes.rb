223c6a0b333f90014e758eb5afbd8beac81b34c3
biankacosta <biankacosta@alu.ufc.br>
2024-09-10 23:34:18 -0300

    Login, log out and User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
 
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   get "/", to: "main#index"
+  get "root", to: "main#index"
   #Also works: root to: "main#index"
 end

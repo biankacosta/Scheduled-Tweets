da1fff6d042f112c27283b8e4caa0a4145804a0c
biankacosta <biankacosta@alu.ufc.br>
2024-09-11 23:03:06 -0300

    Accessing the Current User globally

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "sign-up", to: "registrations#new"
   post "sign-up", to: "registrations#create"
+
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
   get "/", to: "main#index"

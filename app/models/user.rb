8fedfadbcb24d07267a2f821d28c145fed9f9acb
biankacosta <biankacosta@alu.ufc.br>
2024-09-12 18:27:57 -0300

    Password Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid e-mail address"}
+    validates :password, presence: true
 end
